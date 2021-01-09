179cf34ff2cf03773e222f722ab426b07a6e24d9
jacopastorius <joseph.bulbulia@gmail.com>
2021-01-09 16:22:24 +1300

    init commit ch 4

---
+++ teaching/notes_statistical_rethinking/libs.R
@@ -0,0 +1,38 @@
+#libs
+# libraries
+
+
+library("rethinking")
+
+# dags
+library("dagitty")
+library("ggdag")
+
+# might be useful
+
+library("tidyverse")
+library("ggthemes")
+library("ggplot2")
+library("ggpubr")
+library("ggsci")
+library("sjstats")
+library("sjPlot")
+library("patchwork")
+library("ggeffects")
+library("brms")
+library("rstan")
+library("bayesplot")
+library("parameters") # great tables, plots
+library("mathjaxr")
+library("prettycode")
+library("styler")
+library("see")# plots
+library("bayestestR") # for latex
+library("performance")
+library("ggpmisc")
+library("mathjaxr")
+# settings
+rstan_options(auto_write=TRUE)
+options(mc.cores=parallel::detectCores ())
+#theme_set(theme_few())
+set_theme(base=theme_sjplot())
